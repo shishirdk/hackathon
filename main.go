692ea782d23762bcf9084e0bf27590c161b161ae
Shishir DK <shishirdk@gmail.com>
2023-08-31 00:21:32 +0530

    CRUD RESTAPI

--- main.go
+++ main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 type Article struct {
+	Id      string `json:"Id"`
 	Title   string `json:"Title"`
 	Desc    string `json:"desc"`
 	Content string `json:"content"`
@@ -15,10 +19,28 @@ type Article struct {
 
 var Articles []Article
 
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
+}
+
 func handleRequests() {
 
-	http.HandleFunc("/articles", returnAllArticles)
-	log.Fatal(http.ListenAndServe(":10000", nil))
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/all", returnAllArticles)
+	myRouter.HandleFunc("/article/{id}", returnSingleArticle)
+	myRouter.HandleFunc("/article", createNewArticle).Methods("POST")
+	myRouter.HandleFunc("/article/{id}", deleteArticle).Methods("DELETE")
+	log.Fatal(http.ListenAndServe(":10000", myRouter))
+}
+
+func createNewArticle(w http.ResponseWriter, r *http.Request) {
+	reqBody, _ := io.ReadAll(r.Body)
+	var article Article
+	json.Unmarshal(reqBody, &article)
+	Articles = append(Articles, article)
+	json.NewEncoder(w).Encode(article)
 }
 
 func returnAllArticles(w http.ResponseWriter, r *http.Request) {
@@ -26,11 +48,34 @@ func returnAllArticles(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(Articles)
 }
 
+func deleteArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	for index, article := range Articles {
+		if article.Id == id {
+			Articles = append(Articles[:index], Articles[index+1:]...)
+		}
+	}
+}
+
+func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["id"]
+
+	fmt.Fprint(w, "Key:"+key)
+	for _, article := range Articles {
+		if article.Id == key {
+			json.NewEncoder(w).Encode(article)
+		}
+	}
+}
+
 func main() {
 
 	Articles = []Article{
-		Article{Title: "Earphone", Desc: "In Ear earphone", Content: "1 Ear phone"},
-		Article{Title: "Airpod", Desc: "Apple Airpods", Content: "Wireless Apple airpod"},
+		Article{Id: "1", Title: "Earphone", Desc: "In Ear earphone", Content: "1 Ear phone"},
+		Article{Id: "2", Title: "Airpod", Desc: "Apple Airpods", Content: "Wireless Apple airpod"},
 	}
 	handleRequests()
 
