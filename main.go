069af4a965379cdb85034815aefb6c34521ff345
Shishir DK <shishirdk@gmail.com>
2023-08-30 23:18:32 +0530

    commiting api test code

--- main.go
+++ main.go
@@ -1,16 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
 
-func main() {
-	a := addTwoNumber(10, 6)
-	fmt.Println("First GoLang Code abd total is :", a)
-
+type Article struct {
+	Title   string `json:"Title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
 }
 
-func addTwoNumber(a, b int) int {
-	one := a
-	two := b
-	total := one + two
-	return total
+var Articles []Article
+
+func handleRequests() {
+
+	http.HandleFunc("/articles", returnAllArticles)
+	log.Fatal(http.ListenAndServe(":10000", nil))
+}
+
+func returnAllArticles(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: returnAllArticles")
+	json.NewEncoder(w).Encode(Articles)
+}
+
+func main() {
+
+	Articles = []Article{
+		Article{Title: "Earphone", Desc: "In Ear earphone", Content: "1 Ear phone"},
+		Article{Title: "Airpod", Desc: "Apple Airpods", Content: "Wireless Apple airpod"},
+	}
+	handleRequests()
+
 }
